a7216cc71ecdbc7d3b605f79c9b70cd9cfc5580e
beast462 <binhmattroi@gmail.com>
2022-04-25 17:46:24 +0700

    Add cookie & session

--- apps/healthy-first/src/main.ts
+++ apps/healthy-first/src/main.ts
@@ -1,10 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { randomBytes } from 'crypto';
+import session from 'express-session';
 import { AppModule } from './app.module';
 import { ConfigKeys } from './base/config.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: randomBytes(32).toString('hex'),
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 
   const configService = app.get(ConfigService);
 

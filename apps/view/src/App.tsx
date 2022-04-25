95fb7206806a52fcf039941466fc1a80d7b5538c
beast462 <binhmattroi@gmail.com>
2022-04-25 12:54:15 +0700

    Add frontend routing; Add NotFound page

--- apps/view/src/App.tsx
+++ apps/view/src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <>Hello world!</>;
+import { connect, ConnectedProps } from 'react-redux';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound';
+import { ApplicationState } from './store';
+
+const connector = connect((_state: ApplicationState) => ({}), {});
+
+function App(_props: ConnectedProps<typeof connector>) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App;
+export default connector(App);

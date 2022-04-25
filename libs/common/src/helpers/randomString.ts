fc8c6491424c4810aef3d182a9c25e3c1ab73d8f
beast462 <binhmattroi@gmail.com>
2022-04-25 14:06:19 +0700

    Add random helpers

---
+++ libs/common/src/helpers/randomString.ts
@@ -0,0 +1,15 @@
+import { randomRange } from './randomRange';
+
+const defaultCharset = '';
+
+export function randomString(
+  length: number,
+  charset: string = defaultCharset,
+): string {
+  const result = [];
+
+  for (let i = 0; i < length; i++)
+    result.push(charset.charAt(randomRange(0, charset.length)));
+
+  return result.join('');
+}

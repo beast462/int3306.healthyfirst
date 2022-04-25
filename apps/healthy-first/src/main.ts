4f919980c60a357356e3b9d57a4a41d033c2da69
beast462 <binhmattroi@gmail.com>
2022-04-25 21:29:20 +0700

    Remove session as it can cause memory leak

--- apps/healthy-first/src/main.ts
+++ apps/healthy-first/src/main.ts
@@ -1,8 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import { randomBytes } from 'crypto';
-import * as session from 'express-session';
 import { AppModule } from './app.module';
 import { ConfigKeys } from './base/config.module';
 
@@ -10,13 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-  app.use(
-    session({
-      secret: randomBytes(32).toString('hex'),
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
 
   const configService = app.get(ConfigService);
 

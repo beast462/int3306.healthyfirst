4723d6b067c81630d5d50247b1c3d35e7d17f78b
beast462 <binhmattroi@gmail.com>
2022-04-27 10:11:07 +0700

    Rename messages -> message for uniform response with validations

--- libs/common/src/dto/response.dto.ts
+++ libs/common/src/dto/response.dto.ts
@@ -2,12 +2,12 @@ import { HttpStatus } from '@nestjs/common';
 
 export class ResponseDTO<BodyType> {
   status: HttpStatus;
-  messages: string[];
+  message: string[];
   body?: BodyType;
 
-  constructor(status: HttpStatus, messages: string[], body?: BodyType) {
+  constructor(status: HttpStatus, message: string[], body?: BodyType) {
     this.status = status;
-    this.messages = messages;
+    this.message = message;
     this.body = body;
   }
 }

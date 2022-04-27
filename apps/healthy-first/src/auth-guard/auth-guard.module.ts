475f151c3e6df0bf11765fa4324da235209bd1ff
beast462 <binhmattroi@gmail.com>
2022-04-27 12:27:26 +0700

    Add AuthGuardMiddleware to providers to prevent error when importing AuthGuardModule

--- apps/healthy-first/src/auth-guard/auth-guard.module.ts
+++ apps/healthy-first/src/auth-guard/auth-guard.module.ts
@@ -5,6 +5,7 @@ import { AuthGuardMiddleware } from './auth-guard.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [AuthGuardMiddleware],
   exports: [AuthGuardMiddleware],
 })
 export class AuthGuardModule {}

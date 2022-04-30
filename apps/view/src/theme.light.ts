dd1bd7ff65ac64df7399381e3d5a471e59853f1c
beast462 <binhmattroi@gmail.com>
2022-04-30 14:20:28 +0700

    Update main colors

--- apps/view/src/theme.light.ts
+++ apps/view/src/theme.light.ts
@@ -1,3 +1,13 @@
 import { createTheme } from '@mui/material';
+import { lightBlue } from '@mui/material/colors';
+import { breakpoints } from './constants/breakpoints';
 
-export const theme = createTheme({});
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: lightBlue[500],
+      contrastText: '#ffffff',
+    },
+  },
+  breakpoints,
+});

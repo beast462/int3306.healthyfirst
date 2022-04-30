d16782700f1d1047394febde01c35636296fe4bf
beast462 <binhmattroi@gmail.com>
2022-04-30 14:20:00 +0700

    Update breakpoints to Bootstrap's breakpoints

--- apps/view/src/constants/breakpoints.ts
+++ apps/view/src/constants/breakpoints.ts
@@ -3,9 +3,9 @@ import { BreakpointsOptions } from '@mui/material';
 export const breakpoints: BreakpointsOptions = {
   values: {
     xs: 0,
-    sm: 600,
-    md: 900,
-    lg: 1200,
-    xl: 1536,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
   },
 };

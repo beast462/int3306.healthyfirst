2d9f08c58999e091aab3032e80f0592e362b7f47
beast462 <binhmattroi@gmail.com>
2022-05-01 10:37:57 +0700

    Update Contents: add content routing

--- apps/view/src/components/Dashboard/Contents/Contents.tsx
+++ apps/view/src/components/Dashboard/Contents/Contents.tsx
@@ -1,7 +1,34 @@
-import { ReactElement } from 'react';
+import styled from '@emotion/styled';
+import { ReactElement, useMemo } from 'react';
+import { Route, Routes } from 'react-router';
+import { Navigations } from '../../Navigations';
+
+const Root = styled.div`
+  flex: 1;
+  height: 100%;
+  padding: 0.5rem 0.75rem;
+`;
 
 function Contents(): ReactElement {
-  return <></>;
+  const navigations = useMemo(() => {
+    const allMenu = Navigations.getInstance().getAllNavigations();
+
+    return allMenu;
+  }, []);
+
+  return (
+    <Root>
+      <Routes>
+        {navigations.map(([path, { element: Element }]) => (
+          <Route
+            key={`route.contents@${path}`}
+            path={path}
+            element={<Element />}
+          />
+        ))}
+      </Routes>
+    </Root>
+  );
 }
 
 export default Contents;

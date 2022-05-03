be1ae16230fe962d031b7c29cea9a8a676c667fd
beast462 <binhmattroi@gmail.com>
2022-05-03 09:27:52 +0700

    Update LoginBodyDTO errors order

--- libs/common/src/dto/user/login.body.dto.ts
+++ libs/common/src/dto/user/login.body.dto.ts
@@ -1,9 +1,9 @@
 import { IsString, Length } from 'class-validator';
 
 export class LoginBodyDTO {
-  @IsString()
   @Length(64, 64, {
     message: 'Answer must be equal to 64 characters',
   })
+  @IsString()
   answer!: string;
 }

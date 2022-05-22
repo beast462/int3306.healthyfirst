4c3217db1bc0fa77e8740c22ec57db4ec605bc0c
beast462 <binhmattroi@gmail.com>
2022-05-23 01:02:56 +0700

    Add role service & role module to user module

---
+++ apps/healthy-first/src/user/role.service.ts
@@ -0,0 +1,18 @@
+import { RoleEntity } from '@/common/entities';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class RoleService {
+  public constructor(
+    @InjectRepository(RoleEntity)
+    private readonly roleRepository: Repository<RoleEntity>,
+  ) {}
+
+  public async getRoleById(id: number): Promise<RoleEntity> {
+    return await this.roleRepository.findOne({
+      where: { id },
+    });
+  }
+}
--- apps/healthy-first/src/user/user.module.ts
+++ apps/healthy-first/src/user/user.module.ts
@@ -1,21 +1,24 @@
-import { UserEntity } from '@/common/entities';
+import { RoleEntity, UserEntity } from '@/common/entities';
 import { byMinutes } from '@/common/helpers/timespan';
 import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailModule } from '../mail/mail.module';
 
+import { RoleService } from './role.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([UserEntity, RoleEntity]),
     ConfigModule,
     CacheModule.register({
       ttl: byMinutes(5),
     }),
+    MailModule,
   ],
-  providers: [UserService],
+  providers: [UserService, RoleService],
   controllers: [UserController],
 })
 export class UserModule {}

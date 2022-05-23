0edee45a2dcc1327d2e4b78ed965d9463a3814a3
beast462 <binhmattroi@gmail.com>
2022-05-23 11:26:42 +0700

    Centralize http error messages (auth guard middleware)

--- libs/common/src/messages/http-error.ts
+++ libs/common/src/messages/http-error.ts
@@ -1,4 +1,8 @@
 export const HttpErrorMessages = {
+  /* Auth guard middleware */
+  AUTH_TOKEN_MISSING: ['required auth token not found'],
+  AUTH_TOKEN_INVALID: ['invalid auth token'],
+  USER_IN_AUTH_TOKEN_NOT_FOUND: ["user in auth token's payload not found"],
   /* User controller */
   USERNAME_DOES_NOT_EXIST: ['username does not exist'],
   LOGIN_CHALLENGE_NOT_FOUND: ['login challenge not found'],

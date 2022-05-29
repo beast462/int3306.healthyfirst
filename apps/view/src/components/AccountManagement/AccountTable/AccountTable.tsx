bdedface313695b46bc004f78cd06c13e5fdd2a5
PhongVan <fmlphongvan@gmail.com>
2022-05-29 23:51:32 +0700

    Update AccountTable: add table's content

--- apps/view/src/components/AccountManagement/AccountTable/AccountTable.tsx
+++ apps/view/src/components/AccountManagement/AccountTable/AccountTable.tsx
@@ -1,7 +1,38 @@
-import { Box, Paper } from '@mui/material';
-import { ReactElement } from 'react';
+import { ApplicationState } from '@/view/store';
+import { changePage as _changePage } from '@/view/store/actions/accountTable/changePage';
+import { changeRpp as _changeRpp } from '@/view/store/actions/accountTable/changeRPP';
+import { Add } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TablePagination,
+} from '@mui/material';
+import React, { ReactElement } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import TableHeader from './TableHeader/TableHeader';
 
-function AccountTable(): ReactElement {
+const connector = connect(
+  (state: ApplicationState) => ({
+    rowsPerPage: state.accountsTable.rowsPerPage,
+    page: state.accountsTable.page,
+    itemsCount: state.accountsTable.itemsCount,
+  }),
+  {
+    changePage: _changePage,
+    changeRpp: _changeRpp,
+  },
+);
+
+function AccountTable({
+  rowsPerPage,
+  page,
+  itemsCount,
+  changePage,
+  changeRpp,
+}: ConnectedProps<typeof connector>): ReactElement {
   return (
     <Box width="100%">
       <Paper>
@@ -11,8 +42,47 @@ function AccountTable(): ReactElement {
           width="100%"
           justifyContent="flex-end"
         >
-          dádấdsđá
+          <Button variant="contained">
+            <Add /> Thêm tài khoản
+          </Button>
         </Box>
+
+        <Table>
+          <TableHeader />
+          <TableBody>qưeqeqưeqưeqưeưqqưeqưeqưeqưeqêqưeqưeqưe</TableBody>
+        </Table>
+
+        <TablePagination
+          component="div"
+          rowsPerPageOptions={[5, 10, 15]}
+          rowsPerPage={rowsPerPage}
+          count={itemsCount}
+          page={page}
+          // onPageChange={async (_, newPage) => {
+          //   await getStudents(rowsPerPage, newPage);
+          //   changePage(newPage);
+          // }}
+          // onRowsPerPageChange={async (event) => {
+          //   const newRpp = event.target.value as unknown as number;
+          //   await getStudents(newRpp, page);
+          //   changeRpp(newRpp);
+          // }}
+          onPageChange={(event: unknown, page: number) => {
+            changePage(page);
+          }}
+          onRowsPerPageChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            const newRpp = +event.target.value;
+            changeRpp(newRpp);
+          }}
+          labelDisplayedRows={({ from, to, count }) =>
+            `${from}–${to} trong ${count !== -1 ? count : `nhiều hơn ${to}`}`
+          }
+          labelRowsPerPage="Số dòng trong 1 trang"
+          nextIconButtonProps={{
+            title: 'Trang tiếp',
+          }}
+          backIconButtonProps={{ title: 'Trang trước' }}
+        />
       </Paper>
     </Box>
   );

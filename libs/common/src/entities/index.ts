de3abf597aade77c9e2001e41527a74727958e28
beast462 <binhmattroi@gmail.com>
2022-05-29 15:59:27 +0700

    Add log entity

--- libs/common/src/entities/index.ts
+++ libs/common/src/entities/index.ts
@@ -1,6 +1,7 @@
 import { RoleEntity } from './role.entity';
 import { UserEntity } from './user.entity';
+import { LogEntity } from './log.entity';
 
-export const Entities = [RoleEntity, UserEntity];
+export const Entities = [RoleEntity, UserEntity, LogEntity];
 
-export { RoleEntity, UserEntity };
+export { RoleEntity, UserEntity, LogEntity };

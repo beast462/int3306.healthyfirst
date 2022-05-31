ea2610e66ac5614f754bfebe8b0260b7a179a526
beast462 <binhmattroi@gmail.com>
2022-05-31 12:30:31 +0700

    Update user entity constraints

--- libs/common/src/entity-constraints/user.entity-constraint.ts
+++ libs/common/src/entity-constraints/user.entity-constraint.ts
@@ -4,5 +4,5 @@ export const MAX_PASSWORD_LENGTH = 64;
 export const MIN_USERNAME_LENGTH = 3;
 export const MAX_USERNAME_LENGTH = 32;
 
-export const UP_VALID_CHARSET =
-  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-_=+./';
+export const MIN_DISPLAY_NAME_LENGTH = 8;
+export const MAX_DISPLAY_NAME_LENGTH = 64;

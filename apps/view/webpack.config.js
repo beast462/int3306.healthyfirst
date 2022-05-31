44fbb6767d80bdf18efdb183363fa1e7111bfb5b
beast462 <binhmattroi@gmail.com>
2022-05-31 12:20:13 +0700

    Add legacy decorator support for frontend

--- apps/view/webpack.config.js
+++ apps/view/webpack.config.js
@@ -128,16 +128,12 @@ const config = {
             plugins: [
               '@babel/plugin-transform-runtime',
               '@babel/plugin-transform-typescript',
+              ['@babel/plugin-proposal-decorators', { legacy: true }],
               '@babel/plugin-proposal-class-properties',
               '@babel/plugin-proposal-nullish-coalescing-operator',
               '@babel/plugin-proposal-optional-chaining',
               '@babel/plugin-transform-async-to-generator',
-              [
-                '@babel/plugin-transform-react-jsx',
-                {
-                  runtime: 'automatic',
-                },
-              ],
+              ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
             ],
           },
         },

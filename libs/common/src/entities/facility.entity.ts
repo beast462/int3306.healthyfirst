f968dc729c23b353629b4687f6d9020a49dc9059
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-04 14:55:10 +0700

    Add facility entity

---
+++ libs/common/src/entities/facility.entity.ts
@@ -0,0 +1,95 @@
+import { IsPhoneNumber } from 'class-validator';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import {
+  MAX_ADDRESS_LENGTH,
+  MAX_PHONE_LENGTH,
+} from '../entity-constraints/common.entity-constraint';
+import { MAX_FACILITY_TYPE_NAME_LENGTH } from '../entity-constraints/facility-type.entity-constraints';
+import {
+  MAX_FACILITY_NAME_LENGTH,
+  MAX_OWNER_NAME_LENGTH,
+} from '../entity-constraints/facility.entitty-constraints';
+import { FacilityTypeEntity } from './facility-type.entity';
+import { LocationEntity } from './location.entity';
+
+export const TABLE_NAME = 'facility';
+
+@Entity(TABLE_NAME)
+export class FacilityEntity {
+  @PrimaryGeneratedColumn({ name: 'id', type: 'int', unsigned: true })
+  id!: number;
+
+  @Column('varchar', {
+    nullable: false,
+    name: 'name',
+    length: MAX_FACILITY_NAME_LENGTH,
+  })
+  name!: string;
+
+  @Column('varchar', {
+    nullable: false,
+    name: 'owner_name',
+    length: MAX_OWNER_NAME_LENGTH,
+  })
+  ownerName!: string;
+
+  @Column('varchar', {
+    nullable: false,
+    name: 'facility_type_name',
+    length: MAX_FACILITY_TYPE_NAME_LENGTH,
+  })
+  facilityTypeName!: string;
+
+  @OneToOne(() => FacilityTypeEntity, (facilityType) => facilityType.name, {
+    nullable: false,
+    onDelete: 'RESTRICT',
+    onUpdate: 'CASCADE',
+    eager: false,
+  })
+  @JoinColumn({
+    name: 'facility_type_name',
+    referencedColumnName: 'name',
+  })
+  facilityType!: FacilityTypeEntity;
+
+  @Column('int', {
+    nullable: false,
+    name: 'facility_location_code',
+    unsigned: true,
+  })
+  facilityLocationCode!: number;
+
+  @ManyToOne(() => LocationEntity, (location) => location.code, {
+    nullable: false,
+    onDelete: 'RESTRICT',
+    onUpdate: 'CASCADE',
+    eager: false,
+  })
+  @JoinColumn({
+    name: 'facility_location_code',
+    referencedColumnName: 'code',
+  })
+  facilityLocation!: LocationEntity;
+
+  @Column('varchar', {
+    nullable: false,
+    name: 'address',
+    length: MAX_ADDRESS_LENGTH,
+  })
+  address!: string;
+
+  @IsPhoneNumber('VN')
+  @Column('varchar', {
+    nullable: false,
+    name: 'phone',
+    length: MAX_PHONE_LENGTH,
+  })
+  phone!: string;
+}

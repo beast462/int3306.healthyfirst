97f3697236de6f40092d3b48a859386310d3b91a
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-04 16:58:00 +0700

    Update index: import foodsamplecrit and foodsample

--- libs/common/src/entities/index.ts
+++ libs/common/src/entities/index.ts
@@ -4,6 +4,8 @@ import { CheckingPlanEntity } from './checking-plan.entity';
 import { FacilityTypeEntity } from './facility-type.entity';
 import { FacilityEntity } from './facility.entity';
 import { FoodSafetyAuthorityBranchEntity } from './food-safety-authority-branch.entity';
+import { FoodSampleCriteriaEntity } from './food-sample-criteria.entity';
+import { FoodSampleEntity } from './food-sample.entity';
 import { InspectionUnitEntity } from './inspection-unit';
 import { LocationEntity } from './location.entity';
 import { LogEntity } from './log.entity';
@@ -14,6 +16,8 @@ import { UserEntity } from './user.entity';
 import { ViolationEntity } from './violation.entity';
 
 export const Entities = [
+  FoodSampleCriteriaEntity,
+  FoodSampleEntity,
   InspectionUnitEntity,
   CheckingActivityEntity,
   PurposeEntity,
@@ -31,6 +35,8 @@ export const Entities = [
 ];
 
 export {
+  FoodSampleCriteriaEntity,
+  FoodSampleEntity,
   InspectionUnitEntity,
   CheckingActivityEntity,
   PurposeEntity,

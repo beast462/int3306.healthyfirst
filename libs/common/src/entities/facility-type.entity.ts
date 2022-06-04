a2c1ac300e32e8af848550e5d08a428dbd79572d
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-04 14:00:43 +0700

    Add facility type entity

---
+++ libs/common/src/entities/facility-type.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { MAX_FACILITY_TYPE_NAME_LENGTH } from '../entity-constraints/facility-type.entity-constraints';
+
+export const TABLE_NAME = 'facility_types';
+
+@Entity(TABLE_NAME)
+export class FacilityTypeEntity {
+  @PrimaryGeneratedColumn({ name: 'id', type: 'int', unsigned: true })
+  id!: number;
+
+  @Column('varchar', {
+    nullable: false,
+    name: 'name',
+    length: MAX_FACILITY_TYPE_NAME_LENGTH,
+  })
+  name!: string;
+}

f1bcb7639917645e2122c5316831053ddb9082f8
PhongVan <fmlphongvan@gmail.com>
2022-06-04 14:26:16 +0700

    Update RegistrationForm: add fetch create new account

--- apps/view/src/components/AccountManagement/RegistrationForm/RegistrationForm.tsx
+++ apps/view/src/components/AccountManagement/RegistrationForm/RegistrationForm.tsx
@@ -7,6 +7,12 @@ import { Button, Divider, Paper, Toolbar, Typography } from '@mui/material';
 import { ISegmentProps } from '../AccountManagement';
 import Inputs from './Inputs/Inputs';
 import Flexbox from '@/view/common/components/Flexbox';
+import { ApplicationState } from '@/view/store';
+import { connect, ConnectedProps } from 'react-redux';
+import { notify } from '@/view/store/actions/app/notify';
+import { NotificationSeverity } from '@/view/common/types/Notification';
+import { HttpStatus } from '@nestjs/common/enums';
+import { ErrorCodes } from '@/common/constants/error-codes';
 
 const Root = styled.div`
   width: 100%;
@@ -23,15 +29,75 @@ const BtnContainer = styled(Flexbox)`
   padding: 1rem 2rem;
 `;
 
-function RegistrationForm({ switchSegment }: ISegmentProps) {
-  const handleSubmit = (event: SyntheticEvent) => {
+const connector = connect((state: ApplicationState) => ({}), {
+  notify,
+});
+
+function RegistrationForm({
+  switchSegment,
+  notify,
+}: ISegmentProps & ConnectedProps<typeof connector>) {
+  const handleSubmit = async (event: SyntheticEvent) => {
     event.preventDefault();
 
     const target = event.target as HTMLFormElement;
 
-    const { username, displayName, email, roleId } = target;
+    const newUser = {
+      username: target.username.value,
+      displayName: target.displayName.value,
+      email: target.email.value,
+      role: +target.roleId.value,
+    };
 
-    console.log(username, displayName, email, roleId);
+    console.log(newUser);
+
+    const { statusCode, message, body, errorCode } = await fetch('/api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    }).then((res) => res.json());
+
+    if (
+      statusCode === HttpStatus.CREATED ||
+      statusCode === HttpStatus.NOT_IMPLEMENTED
+    ) {
+      notify('Tạo tài khoản thành công', NotificationSeverity.SUCCESS);
+
+      target.username.value = '';
+      target.displayName.value = '';
+      target.email.value = '';
+    } else {
+      const errorMessages = [];
+      switch (errorCode) {
+        case ErrorCodes.USERNAME_ALREADY_EXISTS:
+          errorMessages.push('Tên đăng nhập đã được đăng ký');
+          break;
+
+        case ErrorCodes.EMAIL_ALREADY_EXISTS:
+          errorMessages.push('Email đã được đăng ký');
+          break;
+
+        case ErrorCodes.USERNAME_AND_EMAIL_ALREADY_EXIST:
+          errorMessages.push('Tên đăng nhập và Email đã được đăng ký');
+          break;
+
+        case ErrorCodes.ROLE_DOES_NOT_EXIST:
+          errorMessages.push('Không tồn tại vai trò này');
+          break;
+
+        default:
+          errorMessages.push('Lỗi không xác định');
+      }
+
+      notify(
+        'Tạo tài khoản không thành công',
+        NotificationSeverity.ERROR,
+        errorMessages,
+      );
+    }
   };
 
   return (
@@ -67,4 +133,4 @@ function RegistrationForm({ switchSegment }: ISegmentProps) {
   );
 }
 
-export default RegistrationForm;
+export default connector(RegistrationForm);

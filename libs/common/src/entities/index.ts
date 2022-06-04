b39e0de36b718e4e057c5a52dce6d1fa77bca88b
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-04 15:53:46 +0700

    Update index: import checkplan, violation, purpose

--- libs/common/src/entities/index.ts
+++ libs/common/src/entities/index.ts
@@ -1,14 +1,20 @@
 import { CertificateEntity } from './certificate.entity';
+import { CheckingPlanEntity } from './checking-plan.entity';
 import { FacilityTypeEntity } from './facility-type.entity';
 import { FacilityEntity } from './facility.entity';
 import { FoodSafetyAuthorityBranchEntity } from './food-safety-authority-branch.entity';
 import { LocationEntity } from './location.entity';
 import { LogEntity } from './log.entity';
+import { PurposeEntity } from './purpose.entity';
 import { ResponsibleAreaEntity } from './responsible-area.entity';
 import { RoleEntity } from './role.entity';
 import { UserEntity } from './user.entity';
+import { ViolationEntity } from './violation.entity';
 
 export const Entities = [
+  PurposeEntity,
+  ViolationEntity,
+  CheckingPlanEntity,
   CertificateEntity,
   FacilityEntity,
   FacilityTypeEntity,
@@ -21,6 +27,9 @@ export const Entities = [
 ];
 
 export {
+  PurposeEntity,
+  ViolationEntity,
+  CheckingPlanEntity,
   CertificateEntity,
   FacilityEntity,
   FacilityTypeEntity,

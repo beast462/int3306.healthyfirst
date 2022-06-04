efa0b3021d9bdf5d39c8368b0a284569e6e02f4c
beast462 <binhmattroi@gmail.com>
2022-06-04 12:43:51 +0700

    Add log entity constraint

--- libs/common/src/entities/log.entity.ts
+++ libs/common/src/entities/log.entity.ts
@@ -1,4 +1,10 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  MAX_FINGERPRINT_LENGTH,
+  MAX_MESSAGE_LENGTH,
+  MAX_SCOPE_LENGTH,
+  MAX_SOURCE_LENGTH,
+} from '../entity-constraints/log.entity-constraint';
 
 export enum LogTypes {
   FATAL = 0,
@@ -33,10 +39,18 @@ export class LogEntity {
   })
   type!: number;
 
-  @Column('varchar', { nullable: false, name: 'scope', length: 20 })
+  @Column('varchar', {
+    nullable: false,
+    name: 'scope',
+    length: MAX_SCOPE_LENGTH,
+  })
   scope!: string;
 
-  @Column('varchar', { nullable: false, name: 'message', length: 64 })
+  @Column('varchar', {
+    nullable: false,
+    name: 'message',
+    length: MAX_MESSAGE_LENGTH,
+  })
   message!: string;
 
   @Column('text', { nullable: false, name: 'stack' })
@@ -55,10 +69,14 @@ export class LogEntity {
   @Column('varchar', {
     nullable: false,
     name: 'fingerprint',
-    length: 128,
+    length: MAX_FINGERPRINT_LENGTH,
   })
   fingerprint!: string;
 
-  @Column('varchar', { nullable: false, name: 'source', length: 20 })
+  @Column('varchar', {
+    nullable: false,
+    name: 'source',
+    length: MAX_SOURCE_LENGTH,
+  })
   source!: string;
 }

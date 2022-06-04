72b82273484c147d1d532e2ac0af24247d61ffa0
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-04 13:45:03 +0700

    Update index: export responsible area entity

--- libs/common/src/entities/index.ts
+++ libs/common/src/entities/index.ts
@@ -1,10 +1,12 @@
 import { FoodSafetyAuthorityBranchEntity } from './food-safety-authority-branch.entity';
 import { LocationEntity } from './location.entity';
 import { LogEntity } from './log.entity';
+import { ResponsibleAreaEntity } from './responsible-area.entity';
 import { RoleEntity } from './role.entity';
 import { UserEntity } from './user.entity';
 
 export const Entities = [
+  ResponsibleAreaEntity,
   FoodSafetyAuthorityBranchEntity,
   LocationEntity,
   LogEntity,
@@ -13,6 +15,7 @@ export const Entities = [
 ];
 
 export {
+  ResponsibleAreaEntity,
   FoodSafetyAuthorityBranchEntity,
   LocationEntity,
   LogEntity,

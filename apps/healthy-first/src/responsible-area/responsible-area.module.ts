3d8ae3b8e19781a412aa365fddbb53a6e8fea1df
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-06 05:04:38 +0700

    Update res area api: add get info user with res location code

---
+++ apps/healthy-first/src/responsible-area/responsible-area.module.ts
@@ -0,0 +1,12 @@
+import { ResponsibleAreaEntity, UserEntity } from '@/common/entities';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ResponsibleAreaController } from './responsible-area.controller';
+import { ResponsibleAreaService } from './responsible-area.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ResponsibleAreaEntity, UserEntity])],
+  controllers: [ResponsibleAreaController],
+  providers: [ResponsibleAreaService],
+})
+export class ResponsibleAreaModule {}

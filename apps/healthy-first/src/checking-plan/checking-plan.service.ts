721d49409cb9d8c056fbeaef52b7035852d64abd
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-06 23:59:08 +0700

    Add checking plan crud api

---
+++ apps/healthy-first/src/checking-plan/checking-plan.service.ts
@@ -0,0 +1,47 @@
+import { CheckingPlanEntity } from '@/common/entities';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CheckingPlanService {
+  constructor(
+    @InjectRepository(CheckingPlanEntity)
+    private readonly checkingPlanRepository: Repository<CheckingPlanEntity>,
+  ) {}
+
+  public async getAllCheckingPlans(): Promise<CheckingPlanEntity[]> {
+    return this.checkingPlanRepository.find();
+  }
+
+  public async getCheckingPlanById(id: number): Promise<CheckingPlanEntity> {
+    return this.checkingPlanRepository.findOne({
+      where: { id: id },
+    });
+  }
+
+  public async modifyCheckingPlan(
+    modifiedCheckingPlan: Omit<CheckingPlanEntity, 'facility'>,
+  ): Promise<Omit<CheckingPlanEntity, 'facility'>> {
+    await this.checkingPlanRepository.update(
+      { id: modifiedCheckingPlan.id },
+      modifiedCheckingPlan,
+    );
+
+    return modifiedCheckingPlan;
+  }
+
+  public async createCheckingPlan(
+    checkingPlan: Omit<CheckingPlanEntity, 'id' | 'facility'>,
+  ): Promise<CheckingPlanEntity> {
+    return (await this.checkingPlanRepository.insert(checkingPlan))
+      .generatedMaps[0] as CheckingPlanEntity;
+  }
+
+  public async deleteCheckingPlan(
+    removedCheckingPlan: CheckingPlanEntity,
+  ): Promise<CheckingPlanEntity> {
+    await this.checkingPlanRepository.remove(removedCheckingPlan);
+    return removedCheckingPlan;
+  }
+}

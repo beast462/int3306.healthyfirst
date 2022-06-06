cbf221ce702a744a2575c5ac6ddf73e865b4a7bd
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-06 17:04:00 +0700

    Update certificate: add get, put api

--- apps/healthy-first/src/certificate/certificate.service.ts
+++ apps/healthy-first/src/certificate/certificate.service.ts
@@ -13,4 +13,54 @@ export class CertificateService {
   public async getAllCertificates(): Promise<CertificateEntity[]> {
     return this.certificateRepository.find();
   }
+
+  public async getCertificateById(id: number): Promise<CertificateEntity> {
+    return this.certificateRepository.findOne({
+      where: { id: id },
+    });
+  }
+
+  public async modifyCertificateById(
+    modifiedCertificate: Omit<CertificateEntity, 'facility'>,
+  ): Promise<Omit<CertificateEntity, 'facility'>> {
+    await this.certificateRepository.update(
+      { id: modifiedCertificate.id },
+      modifiedCertificate,
+    );
+
+    return modifiedCertificate;
+  }
+
+  public async getCertificateByFacilityId(
+    facilityId: number,
+  ): Promise<CertificateEntity> {
+    return this.certificateRepository.findOne({
+      where: { facilityId: facilityId },
+    });
+  }
+
+  public async modifyCertificateByFacilityId(
+    modifiedCertificate: Omit<CertificateEntity, 'facility'>,
+  ): Promise<Omit<CertificateEntity, 'facility'>> {
+    await this.certificateRepository.update(
+      { facilityId: modifiedCertificate.facilityId },
+      modifiedCertificate,
+    );
+
+    return modifiedCertificate;
+  }
+
+  public async createCertificate(
+    newCertificate: Omit<CertificateEntity, 'id' | 'facility'>,
+  ): Promise<CertificateEntity> {
+    return (await this.certificateRepository.insert(newCertificate))
+      .generatedMaps[0] as CertificateEntity;
+  }
+
+  public async deleteCertificate(
+    removedCertificate: CertificateEntity,
+  ): Promise<CertificateEntity> {
+    await this.certificateRepository.remove(removedCertificate);
+    return removedCertificate;
+  }
 }

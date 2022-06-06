43f1025225ad4e1db939f73cabb855d97b8dfbe7
beast462 <binhmattroi@gmail.com>
2022-06-06 11:35:53 +0700

    Fix use managed specialists hook access property on null: add location code check

--- apps/view/src/hooks/useManagedSpecialists.ts
+++ apps/view/src/hooks/useManagedSpecialists.ts
@@ -5,19 +5,21 @@ import useSWR from 'swr';
 import { ErrorCodes } from '@/common/constants/error-codes';
 import { ResponseDTO } from '@/common/dto/response.dto';
 import { SerializableError } from '@/common/models/serializable-error';
+import { Specialist } from '@/common/models/specialist';
 import { HttpStatus } from '@nestjs/common/enums';
 
 import { swrHookKeys } from '../common/constants/swrHookKeys';
 import { NotificationSeverity } from '../common/types/Notification';
 import { notify } from '../store/actions/app/notify';
-import { Specialist } from '@/common/models/specialist';
-import { GetManagedSpecialistsResDTO } from '@/common/dto/specialists/get-managed-specialists-res.dto';
 import { useResponsibleLocation } from './useResponsibleLocation';
 
 async function fetchManagedSpecialists(
   this: Dispatch<any>,
   locationCode: number,
 ): Promise<Specialist[]> {
+  if (typeof locationCode !== 'number')
+    throw new SerializableError(new Error(ErrorCodes.UNKNOWN_ERROR.toString()));
+
   const { statusCode, body, message, errorCode }: ResponseDTO<Specialist[]> =
     await fetch(`/api/responsible-area/users/${locationCode}`).then((res) =>
       res.json(),
@@ -39,19 +41,24 @@ async function fetchManagedSpecialists(
 
 export function useManagedSpecialists() {
   const dispatch = useDispatch();
-  const { responsibleLocationCode } = useResponsibleLocation().data;
-  const { data, error } = useSWR<Specialist[], Error>(
-    swrHookKeys.USE_MANAGED_SPECIALISTS,
-    fetchManagedSpecialists.bind(dispatch, responsibleLocationCode),
-  );
+  const { data: responsibleLocationData } = useResponsibleLocation();
+  const { data: managedSpecialistsData, error: managedSpecialistsError } =
+    useSWR<Specialist[], Error>(
+      swrHookKeys.USE_MANAGED_SPECIALISTS,
+      fetchManagedSpecialists.bind(
+        dispatch,
+        responsibleLocationData?.responsibleLocationCode,
+      ),
+    );
 
   const isError =
-    error &&
-    (error instanceof Error || SerializableError.isSerializableError(error));
+    managedSpecialistsError &&
+    (managedSpecialistsError instanceof Error ||
+      SerializableError.isSerializableError(managedSpecialistsError));
 
   return {
-    data,
-    isLoading: !error && !data,
-    error: isError ? error : null,
+    data: managedSpecialistsData,
+    isLoading: !managedSpecialistsError && !managedSpecialistsData,
+    error: isError ? managedSpecialistsError : null,
   };
 }

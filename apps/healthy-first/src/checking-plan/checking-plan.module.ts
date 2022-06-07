2c8ac7e98509e78175fd4ed1527174d25e4d730d
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-07 18:05:25 +0700

    Update checking plan module: import facility module

--- apps/healthy-first/src/checking-plan/checking-plan.module.ts
+++ apps/healthy-first/src/checking-plan/checking-plan.module.ts
@@ -1,11 +1,12 @@
 import { CheckingPlanEntity } from '@/common/entities';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FacilityModule } from '../facility/facility.module';
 import { CheckingPlanController } from './checking-plan.controller';
 import { CheckingPlanService } from './checking-plan.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CheckingPlanEntity])],
+  imports: [TypeOrmModule.forFeature([CheckingPlanEntity]), FacilityModule],
   controllers: [CheckingPlanController],
   providers: [CheckingPlanService],
 })

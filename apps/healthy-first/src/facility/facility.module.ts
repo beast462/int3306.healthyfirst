e5e8c599847b91ef5dfeb6952f1f2893119b040c
hieuvuhv34 <hieuvuhv34@gmail.com>
2022-06-07 14:02:36 +0700

    Update facility module: import checking plan entity

--- apps/healthy-first/src/facility/facility.module.ts
+++ apps/healthy-first/src/facility/facility.module.ts
@@ -1,11 +1,21 @@
-import { CertificateEntity, FacilityEntity } from '@/common/entities';
+import {
+  CertificateEntity,
+  CheckingPlanEntity,
+  FacilityEntity,
+} from '@/common/entities';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FacilityController } from './facility.controller';
 import { FacilityService } from './facility.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FacilityEntity, CertificateEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      FacilityEntity,
+      CertificateEntity,
+      CheckingPlanEntity,
+    ]),
+  ],
   controllers: [FacilityController],
   providers: [FacilityService],
 })
